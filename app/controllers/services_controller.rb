c2aef31592e89ec8133d7f41ac2dc921f421c70d
Joel Noguera <Noguera.sistemas@gmail.com>
2014-05-25 22:47:58 -0300

    Menu solucionado

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -69,6 +69,6 @@ class ServicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def service_params
-      params.require(:service).permit(:name, :description, :type_of_service_id)
+      params.require(:service).permit(:name, :description)
     end
 end
